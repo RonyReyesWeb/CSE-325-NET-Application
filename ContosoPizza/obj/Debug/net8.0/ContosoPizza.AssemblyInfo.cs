6d8bff6e6dce8ec14d43b05dcd5351d8068b4529
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2026-09-03 11:27:01 -0700

    adding

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d6055a5c0a1bfd5c673d0536b172262aa69056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8948e4b96d37c0deeea725024f96ce5d083f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
